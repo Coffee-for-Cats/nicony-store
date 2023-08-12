aa5bc1dced69d943d4ab1fb17d715d996cb9124b
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-08-12 11:51:23 -0300

    delayed every map function

--- postload.js
+++ postload.js
@@ -1,3 +1,5 @@
 window.onload = function() {
-    document.getElementById("address-google-map").hidden = false;
+    const iframe = document.getElementById("address-google-map");
+    iframe.src = iframe.dataset.src;
+    iframe.hidden = false;
 }
