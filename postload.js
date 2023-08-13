c8c574ddddcac533871977874ac6e5dc404b6b91
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-08-12 21:00:48 -0300

    wait 10ms after ready to show the map

--- postload.js
+++ postload.js
@@ -1,4 +1,8 @@
-window.onload = function() {
+window.onload = async function() {
+    window.setTimeout(loadMap ,10)
+}
+
+function loadMap() {
     const iframe = document.getElementById("address-google-map");
     iframe.src = iframe.dataset.src;
     iframe.hidden = false;
